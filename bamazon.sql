2fd7f81d3e84d540e575432f6e688cd1271dc0a1
Sara McMullin <mcsara1221@gmail.com>
2019-07-23 23:17:21 -0500

    update database numbers

--- bamazon.sql
+++ bamazon.sql
@@ -7,7 +7,7 @@ CREATE TABLE products(
     item_id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR (50) NOT NULL,
     department_name VARCHAR(50) NOT NULL,
-    price Decimal ,
+    price Decimal(5,2) ,
     stock_quantity INT,
      PRIMARY KEY (item_id)
 );
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,34 +1,85 @@
 var mysql = require("mysql");
+var inquirer=require("inquirer")
 
 var connection = mysql.createConnection({
     host: "localhost",
-    port:3306,
+    port: 3306,
     user: "Sara",
     password: "Harley12",
     database: "bamazon_DB"
 });
 
-connection.connect(function(err){
-    if(err) throw err;
+connection.connect(function (err) {
+    if (err) throw err;
     start();
 });
 
-function start(){
-connection.query("SELECT * FROM products", function(err, res){
-    if(err) throw err;
-    console.log(res)
-})
+function start() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+       // console.log(res)
+        console.table(res)
+        menu()
+    })
 }
-// function start(){
-//     inquirer
-//     .prompt({
-//         item_id: "search",
-//         type: "list",
-//         message:""
-        
+
+function menu() {
+    inquirer
+        .prompt([
+            {
+                name: "id",
+                type: "input",
+                message: " What is the Product ID?",
+                validate: function(value){
+                    if(isNaN(value)){
+                        return false
+                    }else{
+                        return true
+                    }
+                }
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: " How many would you like to purchase?",
+                validate: function(value){
+                    if (isNaN(value)){
+                        return false
+                        
+                    }else{
+                        return true
+                    }
+                }
+            },
+        ]).then(function (answer) {
+            // console.log(answer)
+
+            connection.query("select * from products  where item_id = ? ", [answer.id], function(err, result){
+
+                var tableQuantity = result[0].stock_quantity
+                var userinputQuantity = answer.quantity
+                 
+                var finalQuantity =  tableQuantity - userinputQuantity;
+                    
+
+                if (finalQuantity>=0){
+                    connection.query("update products set stock_quantity = ? where item_id = ? ", [finalQuantity,answer.id] , function(err, result){
+                        if (err) throw err
+                        
+                        start()
+                        
+                    })
+                }
+                else{
+                    console.log ("sorry insufficiant quantity selection, please try again")
+                    start()
+                }
+                 
+            })
+
+      
 
 
+          });
+        }
 
-//     })
-
-// }
